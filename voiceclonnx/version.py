c1fecb690d682dbd4ef6e911bb8c3fda0e4ad954
JarbasAl <33701864+JarbasAl@users.noreply.github.com>
2026-07-17 16:27:14 +0000

    Increment Version to 0.0.3a1

--- voiceclonnx/version.py
+++ voiceclonnx/version.py
@@ -1,8 +1,8 @@
 # START_VERSION_BLOCK
 VERSION_MAJOR = 0
 VERSION_MINOR = 0
-VERSION_BUILD = 2
-VERSION_ALPHA = 2
+VERSION_BUILD = 3
+VERSION_ALPHA = 1
 # END_VERSION_BLOCK
 
 # __version__ is derived from the block above so that the release automation
